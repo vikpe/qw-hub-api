9a6ef80f5f3d40ac7947f710906f5161fdbbf5c3
Viktor Persson <viktor.persson@arcsin.se>
2022-06-02 21:52:06 +0200

    Refactor data provider.

--- sources/provider.go
+++ sources/provider.go
@@ -10,27 +10,27 @@ import (
 )
 
 type Provider struct {
-	serverScraper *ServerScraper
-	twitchScraper *TwitchScraper
-	geoDb         GeoDatabase
+	serverSource *ServerScraper
+	twitchSource *TwitchScraper
+	geoDb        GeoDatabase
 }
 
-func NewProvider(serverScraper *ServerScraper, twitchScraper *TwitchScraper, geoDb GeoDatabase) Provider {
+func NewProvider(servers *ServerScraper, twitch *TwitchScraper, geoDb GeoDatabase) Provider {
 	return Provider{
-		serverScraper: serverScraper,
-		twitchScraper: twitchScraper,
-		geoDb:         geoDb,
+		serverSource: servers,
+		twitchSource: twitch,
+		geoDb:        geoDb,
 	}
 }
 
 func (d Provider) Generic() []qserver.GenericServer {
-	return d.serverScraper.Servers()
+	return d.serverSource.Servers()
 }
 
 func (d Provider) Mvdsv() []mvdsv.Mvdsv {
 	result := make([]mvdsv.Mvdsv, 0)
 
-	for _, server := range d.serverScraper.Servers() {
+	for _, server := range d.serverSource.Servers() {
 		if server.Version.IsMvdsv() {
 			mvdsvServer := convert.ToMvdsv(server)
 			mvdsvServer.Geo = d.geoDb.GetByAddress(server.Address)
@@ -44,7 +44,7 @@ func (d Provider) Mvdsv() []mvdsv.Mvdsv {
 func (d Provider) Qtv() []qtv.Qtv {
 	result := make([]qtv.Qtv, 0)
 
-	for _, server := range d.serverScraper.Servers() {
+	for _, server := range d.serverSource.Servers() {
 		if server.Version.IsQtv() {
 			qtvServer := convert.ToQtv(server)
 			qtvServer.Geo = d.geoDb.GetByAddress(server.Address)
@@ -58,7 +58,7 @@ func (d Provider) Qtv() []qtv.Qtv {
 func (d Provider) Qwfwd() []qwfwd.Qwfwd {
 	result := make([]qwfwd.Qwfwd, 0)
 
-	for _, server := range d.serverScraper.Servers() {
+	for _, server := range d.serverSource.Servers() {
 		if server.Version.IsQwfwd() {
 			qwfwdServer := convert.ToQwfwd(server)
 			qwfwdServer.Geo = d.geoDb.GetByAddress(server.Address)
@@ -70,5 +70,5 @@ func (d Provider) Qwfwd() []qwfwd.Qwfwd {
 }
 
 func (d Provider) Streams() []helix.Stream {
-	return d.twitchScraper.Streams
+	return d.twitchSource.Streams
 }
