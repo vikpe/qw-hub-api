f53e642f9c828715543aa34110763171030bcdef
Viktor Persson <viktor.persson@arcsin.se>
2022-06-18 23:14:16 +0200

    Remove obsolete endpoints.

--- api/v2/v2.go
+++ api/v2/v2.go
@@ -12,8 +12,5 @@ func Init(router fiber.Router, provider *sources.Provider) {
 	router.Get("mvdsv", handlers.Mvdsv(provider))
 	router.Get("qtv", handlers.Qtv(provider))
 	router.Get("qwfwd", handlers.Qwfwd(provider))
-	router.Get("mvdsv_to_qtv", handlers.MvdsvToQtv(provider))
-	router.Get("qtv_to_mvdsv", handlers.QtvToMvdsv(provider))
-	router.Get("find_player", handlers.FindPlayer(provider))
 	router.Get("streams", handlers.Streams(provider))
 }
