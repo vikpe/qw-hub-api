d6a0c92daf73ff8be56a631c82911d1c5d8cc18b
Viktor Persson <viktor.persson@arcsin.se>
2022-09-13 22:17:26 +0200

    Refactor v2 routes.

--- internal/api/v2/v2.go
+++ internal/api/v2/v2.go
@@ -2,19 +2,19 @@ package v2
 
 import (
 	"github.com/gofiber/fiber/v2"
-	handlers2 "qws/internal/api/v2/handlers"
+	"qws/internal/api/v2/handlers"
 	"qws/internal/sources"
 )
 
 func Init(router fiber.Router, provider *sources.Provider) {
-	router.Get("servers/mvdsv", handlers2.Mvdsv(provider))
-	router.Get("servers/qtv", handlers2.Qtv(provider))
-	router.Get("servers/qwfwd", handlers2.Qwfwd(provider))
-	router.Get("servers/:address", handlers2.ServerDetails(provider))
-	router.Get("servers", handlers2.Servers(provider))
+	router.Get("servers/mvdsv", handlers.Mvdsv(provider))
+	router.Get("servers/qtv", handlers.Qtv(provider))
+	router.Get("servers/qwfwd", handlers.Qwfwd(provider))
+	router.Get("servers/:address", handlers.ServerDetails(provider))
+	router.Get("servers", handlers.Servers(provider))
 
-	router.Get("streams", handlers2.Streams(provider))
-	router.Get("events", handlers2.Events())
-	router.Get("news", handlers2.News())
-	router.Get("forum_posts", handlers2.ForumPosts())
+	router.Get("streams", handlers.Streams(provider))
+	router.Get("events", handlers.Events())
+	router.Get("news", handlers.News())
+	router.Get("forum_posts", handlers.ForumPosts())
 }
