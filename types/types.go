0590eca40cbf37cab72dab06ac7ca40c07893aae
Viktor Persson <viktor.persson@arcsin.se>
2022-10-27 19:06:43 +0200

    Add QtvServer and QtvHostedDemo.

--- types/types.go
+++ types/types.go
@@ -1,8 +1,120 @@
 package types
 
 import (
+	"fmt"
+	"strings"
 	"time"
+
+	"github.com/goccy/go-json"
 )
+
+type QtvServer struct {
+	Address string
+}
+
+func (s QtvServer) DemoStreamUrl(filename string) string {
+	return fmt.Sprintf("http://%s/watch.qtv?demo=%s", s.Address, filename)
+}
+
+func (s QtvServer) DemoDownloadUrl(filename string) string {
+	return fmt.Sprintf("http://%s/dl/demos/%s", s.Address, filename)
+}
+
+type QtvHostedDemo struct {
+	Filename DemoFileName
+	Server   QtvServer
+}
+
+func (d *QtvHostedDemo) MarshalJSON() ([]byte, error) {
+	type QtvHostedDemoExport struct {
+		Filename      string `json:"filename"`
+		ServerAddress string `json:"server_address"`
+		StreamUrl     string `json:"stream_url"`
+		DownloadUrl   string `json:"download_url"`
+	}
+
+	return json.Marshal(QtvHostedDemoExport{
+		Filename:      string(d.Filename),
+		ServerAddress: d.Server.Address,
+		StreamUrl:     d.StreamUrl(),
+		DownloadUrl:   d.DownloadUrl(),
+	})
+}
+
+func (d *QtvHostedDemo) StreamUrl() string {
+	return d.Server.DemoDownloadUrl(string(d.Filename))
+}
+
+func (d *QtvHostedDemo) DownloadUrl() string {
+	return d.Server.DemoDownloadUrl(string(d.Filename))
+}
+
+type DemoFileName string
+
+func (d DemoFileName) Mode() string {
+	strVal := string(d)
+
+	indexFirstUnderScore := strings.IndexRune(strVal, '_')
+	if -1 == indexFirstUnderScore {
+		return ""
+	}
+
+	return strVal[0:indexFirstUnderScore]
+}
+
+func (d DemoFileName) Participants() []string {
+	strVal := string(d)
+
+	indexFirstUnderScore := strings.IndexRune(strVal, '_')
+	if -1 == indexFirstUnderScore {
+		return make([]string, 0)
+	}
+
+	indexOpenBracket := strings.IndexRune(strVal, '[')
+	if -1 == indexOpenBracket {
+		return make([]string, 0)
+	}
+
+	participantStr := strVal[indexFirstUnderScore+1 : indexOpenBracket]
+
+	const vsNeedle = "_vs_"
+	if strings.Contains(participantStr, vsNeedle) {
+		return strings.SplitN(participantStr, vsNeedle, 2)
+	}
+
+	return []string{participantStr}
+}
+
+func (d DemoFileName) Map() string {
+	strVal := string(d)
+
+	indexOpenBracket := strings.IndexRune(strVal, '[')
+	if -1 == indexOpenBracket {
+		return ""
+	}
+
+	indexCloseBracket := strings.IndexRune(strVal, ']')
+	if -1 == indexCloseBracket {
+		return ""
+	}
+
+	if indexCloseBracket-indexOpenBracket < 1 {
+		return ""
+	}
+
+	return strVal[indexOpenBracket+1 : indexCloseBracket]
+}
+
+func (d DemoFileName) DateTime() string {
+	strVal := string(d)
+
+	indexCloseBracket := strings.IndexRune(strVal, ']')
+	if -1 == indexCloseBracket {
+		return ""
+	}
+
+	return strVal[indexCloseBracket+1 : len(strVal)-len(".mvd")]
+}
 
 type Event struct {
 	Title   string `json:"title"`
---
+++ types/types_test.go
@@ -0,0 +1,42 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+	"github.com/vikpe/qw-hub-api/types"
+)
+
+func TestDemoFileName(t *testing.T) {
+	t.Run("invalid filename", func(t *testing.T) {
+		demoFileName := types.DemoFileName("")
+		assert.Equal(t, "", demoFileName.Mode())
+		assert.Equal(t, make([]string, 0), demoFileName.Participants())
+		assert.Equal(t, "", demoFileName.Map())
+		assert.Equal(t, "", demoFileName.DateTime())
+	})
+
+	t.Run("valid filename", func(t *testing.T) {
+		t.Run("duel", func(t *testing.T) {
+			demoFileName := types.DemoFileName("duel_holy_vs_si7h[bravado]261022-2255.mvd")
+			assert.Equal(t, "duel", demoFileName.Mode())
+			assert.Equal(t, []string{"holy", "si7h"}, demoFileName.Participants())
+			assert.Equal(t, "bravado", demoFileName.Map())
+			assert.Equal(t, "261022-2255", demoFileName.DateTime())
+		})
+
+		t.Run("ffa", func(t *testing.T) {
+			demoFileName := types.DemoFileName("ffa_5[ztndm3]151022-2104.mvd")
+			assert.Equal(t, "ffa", demoFileName.Mode())
+			assert.Equal(t, []string{"5"}, demoFileName.Participants())
+			assert.Equal(t, "ztndm3", demoFileName.Map())
+			assert.Equal(t, "151022-2104", demoFileName.DateTime())
+		})
+	})
+}
+
+func TestQtvServer(t *testing.T) {
+	server := types.QtvServer{Address: "qw.foppa.dk:28000"}
+	assert.Equal(t, "http://qw.foppa.dk:28000/dl/demos/foo.mvd", server.DemoDownloadUrl("foo.mvd"))
+	assert.Equal(t, "http://qw.foppa.dk:28000/watch.qtv?demo=foo.mvd", server.DemoStreamUrl("foo.mvd"))
+}
