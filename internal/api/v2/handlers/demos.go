4372b3f60cd199e0d393a456ce9fffb6e0ec0438
Viktor Persson <viktor.persson@arcsin.se>
2022-10-28 16:29:55 +0200

    Update demo filtering.

--- internal/api/v2/handlers/demos.go
+++ internal/api/v2/handlers/demos.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strings"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/vikpe/qw-hub-api/pkg/qdemo"
@@ -9,6 +11,7 @@ import (
 
 type DemoParams struct {
 	Mode  string `query:"mode" validate:"omitempty"`
+	Query string `query:"q" validate:"omitempty"`
 	Limit int    `query:"limit" validate:"omitempty,gte=1,lte=100"`
 }
 
@@ -34,22 +37,64 @@ func Demos(demoProvider func() []qtvscraper.Demo) func(c *fiber.Ctx) error {
 	}
 }
 
-func FilterDemos(allDemos []qtvscraper.Demo, params *DemoParams) []qtvscraper.Demo {
-	result := make([]qtvscraper.Demo, 0)
-
-	if len(params.Mode) > 0 {
-		for _, demo := range allDemos {
-			if qdemo.Filename(demo.Filename).Mode() == params.Mode {
-				result = append(result, demo)
-			}
-		}
-	} else {
-		result = allDemos
-	}
+func FilterDemos(demos []qtvscraper.Demo, params *DemoParams) []qtvscraper.Demo {
+	result := FilterByMode(demos, params.Mode)
+	result = FilterByQuery(demos, params.Query)
 
 	if params.Limit > 0 && len(result) > params.Limit {
 		result = result[0:params.Limit]
 	}
 
 	return result
+}
+
+func FilterByMode(demos []qtvscraper.Demo, mode string) []qtvscraper.Demo {
+	if 0 == len(mode) {
+		return demos
+	}
+
+	result := make([]qtvscraper.Demo, 0)
+	for _, demo := range demos {
+		if qdemo.Filename(demo.Filename).Mode() == mode {
+			result = append(result, demo)
+		}
+	}
+
+	return result
+}
+
+func FilterByQuery(demos []qtvscraper.Demo, query string) []qtvscraper.Demo {
+	if 0 == len(query) {
+		return demos
+	}
+
+	result := make([]qtvscraper.Demo, 0)
+
+	for _, demo := range demos {
+		if queryMatch(demo.Filename, query) {
+			result = append(result, demo)
+		}
+	}
+
+	return result
+}
+
+func queryMatch(haystack string, query string) bool {
+	if 0 == len(query) {
+		return false
+	}
+
+	if 0 == len(haystack) {
+		return false
+	}
+
+	needles := strings.Split(query, " ")
+
+	for _, needle := range needles {
+		if !strings.Contains(haystack, needle) {
+			return false
+		}
+	}
+
+	return true
 }
