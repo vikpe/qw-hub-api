fcd777ca83f33b5f2bcfc81ed63d497cdbf42a28
Viktor Persson <viktor.persson@arcsin.se>
2023-07-10 18:04:49 +0200

    Fix typo in variable name.

--- internal/api/v2/handlers/wiki_recent_changes.go
+++ internal/api/v2/handlers/wiki_recent_changes.go
@@ -11,13 +11,13 @@ func WikiRecentChanges() func(c *fiber.Ctx) error {
 	const limit = 5
 
 	return func(c *fiber.Ctx) error {
-		newsPosts, err := qwnu.WikiRecentChanges(limit)
+		articles, err := qwnu.WikiRecentChanges(limit)
 
 		if err != nil {
 			return err
 		}
 
 		c.Response().Header.Add("Cache-Time", fmt.Sprintf("%d", 3600)) // 1h cache
-		return c.JSON(newsPosts)
+		return c.JSON(articles)
 	}
 }
